7d688f76127db1982267bdca6052f10525ca91e5
Jonathan Schaller <JJ.latanom42@gmail.com>
2019-02-19 15:13:21 -0800

    print autos didnt push last time for some reason, here they are

---
+++ src/main/java/frc/robot/commands/PrintAutos/PrintAuto2.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.PrintAutos;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Robot;
+// import frc.robot.commands.*;
+import frc.robot.commands.ExtendClimber; 
+
+public class PrintAuto2 extends CommandGroup{
+     public PrintAuto2(){
+          System.out.println("Running PrintAut2");
+          
+      } 
+
+}
---
+++ src/main/java/frc/robot/commands/PrintAutos/PrintAuto3.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.PrintAutos;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Robot;
+// import frc.robot.commands.*;
+import frc.robot.commands.ExtendClimber; 
+
+public class PrintAuto3 extends CommandGroup{
+     public PrintAuto3(){
+          System.out.println("Running PrintAuto3");
+          
+      } 
+
+}
