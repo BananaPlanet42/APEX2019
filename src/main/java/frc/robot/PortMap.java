7c5e62570db758d25b23fe7c8330d3282ada213c
DriveraStation2\Apex5803 <steph@apexfrc.com>
2019-03-02 09:04:54 -0800

    fixed ports better now we think

--- src/main/java/frc/robot/PortMap.java
+++ src/main/java/frc/robot/PortMap.java
@@ -19,8 +19,8 @@ public static int L3 = 4;
 public static int Lift1 = 3;
 public static int Lift2 = 8;
 
-public static int Lift_Solenoid1_Forward_Channel = 5; //2, 5 for lift solenoid
-public static int Lift_Solenoid1_Reverse_Channel = 2;
+public static int Lift_Solenoid1_Forward_Channel = 2; //2, 5 for lift solenoid
+public static int Lift_Solenoid1_Reverse_Channel = 5;
 
 //eater (intake) id, TalonSRX
 public static int Intake = 5;
@@ -31,16 +31,16 @@ public static int Crossbow1_Reverse_Channel = 6;
 
 
 //Climber PDP stuff, solenoid IDs (adjust later)
-public static int ClimberSolenoid1_Forward_Channel = 3; //4, 3 are for climber
-public static int ClimberSolenoid1_Reverse_Channel = 4;
+public static int ClimberSolenoid1_Forward_Channel = 4; //4, 3 are for climber
+public static int ClimberSolenoid1_Reverse_Channel = 3;
 
 //climber speed controller ids,1 is talon 2 is victor
-public static int Climber1 = 7;
-public static int Climber2 = 6;
+public static int Climber1 = 6; //6 for climber 1, 7 for climber 2
+public static int Climber2 = 7;
 
 //climber lock solenoid ids
-public static int ClimberLockSolenoid1_Forward_Channel = 7;
-public static int ClimberLockSolenoid1_Reverse_Channel = 0;
+public static int ClimberLockSolenoid1_Forward_Channel = 0;//0, 7 for limber lock
+public static int ClimberLockSolenoid1_Reverse_Channel = 7;
 public static int Pidgeon = 0;
 
 }
