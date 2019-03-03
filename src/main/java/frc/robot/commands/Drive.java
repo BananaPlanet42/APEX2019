db6d2370179c0be058cc9698aa4650c09bbc64a5
DriveraStation2\Apex5803 <steph@apexfrc.com>
2019-03-02 17:57:40 -0800

    added fine-truning mode to drive, on left bumper

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -1,4 +1,5 @@
 package frc.robot.commands;
+
 import frc.robot.OI;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
@@ -22,42 +23,53 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class Drive extends Command {
 
-	DriveHelper helper;
+    DriveHelper helper;
 
     public Drive() {
-    	helper = new DriveHelper();
+        helper = new DriveHelper();
         System.out.println("gonna define subsystem");
-    	requires(Robot.driveTrain);
-    	System.out.println("required subsystem");
-    	
-    	// Use requires() here to declare subsystem dependencies
+        requires(Robot.driveTrain);
+        System.out.println("required subsystem");
+
+        // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    System.out.println("Initialized drive train in coast mode");
-    	RobotMap.L1.setNeutralMode(NeutralMode.Coast);
-    	RobotMap.L2.setNeutralMode(NeutralMode.Coast);
-    	RobotMap.L3.setNeutralMode(NeutralMode.Coast);
-    	RobotMap.R1.setNeutralMode(NeutralMode.Coast);
-    	RobotMap.R2.setNeutralMode(NeutralMode.Coast);
-    	RobotMap.R3.setNeutralMode(NeutralMode.Coast);
-    	
+        System.out.println("Initialized drive train in coast mode");
+        RobotMap.L1.setNeutralMode(NeutralMode.Coast);
+        RobotMap.L2.setNeutralMode(NeutralMode.Coast);
+        RobotMap.L3.setNeutralMode(NeutralMode.Coast);
+        RobotMap.R1.setNeutralMode(NeutralMode.Coast);
+        RobotMap.R2.setNeutralMode(NeutralMode.Coast);
+        RobotMap.R3.setNeutralMode(NeutralMode.Coast);
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 
- /*   	boolean quickTurn = Robot.driveTrain.quickTurnController();
-    	Robot.driveTrain.drivedriveSignal);
-*/
-		double move = OI.xbox1.getY(Hand.kLeft);
-		double rotate = OI.xbox1.getX(Hand.kRight);
-		boolean quickTurn = Robot.driveTrain.quickTurnController(); 
-		DriveSignal driveSignal = helper.cheesyDrive(1.0 * -move, 0.3 * rotate, quickTurn, false);
-    	Robot.driveTrain.drive(ControlMode.PercentOutput, driveSignal);
+        /*
+         * boolean quickTurn = Robot.driveTrain.quickTurnController();
+         * Robot.driveTrain.drivedriveSignal);
+         */
+
+        if (OI.xbox1.getBumper(Hand.kLeft) == true) {
+            double move = OI.xbox1.getY(Hand.kLeft);
+            double rotate = OI.xbox1.getX(Hand.kRight);
+            boolean quickTurn = Robot.driveTrain.quickTurnController();
+            DriveSignal driveSignal = helper.cheesyDrive(1.0 * -move, 0.15 * rotate, quickTurn, false);
+            Robot.driveTrain.drive(ControlMode.PercentOutput, driveSignal);
+        } else {
+            double move = OI.xbox1.getY(Hand.kLeft);
+            double rotate = OI.xbox1.getX(Hand.kRight);
+            boolean quickTurn = Robot.driveTrain.quickTurnController();
+            DriveSignal driveSignal = helper.cheesyDrive(1.0 * -move, 0.3 * rotate, quickTurn, false);
+            Robot.driveTrain.drive(ControlMode.PercentOutput, driveSignal);
         }
+
+    }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
